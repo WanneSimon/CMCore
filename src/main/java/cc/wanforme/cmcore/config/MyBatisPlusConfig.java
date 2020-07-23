b250965dcb0ce6161e69d5c1ce920cb9faecfc1e
wanne <wanneseville@foxmail.com>
2020-07-23 14:37:19 +0800

    更新gitignore，代码补提

---
+++ src/main/java/cc/wanforme/cmcore/config/MyBatisPlusConfig.java
@@ -0,0 +1,23 @@
+package cc.wanforme.cmcore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+
+/** mybatis-plus部分配置
+ * @author wanne
+ * 2020年7月21日
+ */
+@Configuration
+public class MyBatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+	
+}
