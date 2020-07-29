8577e2507165e7eb15aeff6a0bdeb7756f9d4f75
wanne <wanneseville@foxmail.com>
2020-07-29 17:08:48 +0800

    md5 加密密码

--- src/main/java/cc/wanforme/cmcore/base/service/IUserService.java
+++ src/main/java/cc/wanforme/cmcore/base/service/IUserService.java
@@ -1,6 +1,11 @@
 package cc.wanforme.cmcore.base.service;
 
 import cc.wanforme.cmcore.base.po.User;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.tomcat.util.security.MD5Encoder;
+
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -17,5 +22,14 @@ public interface IUserService extends IService<User> {
 	
 	/**登录检查，只需要用户名和密码*/
 	boolean loginCheck(User user);
+	
+	/** 密码加密*/
+	default String encodePassword(String password) {
+		try {
+			return MD5Encoder.encode(password.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			return MD5Encoder.encode(password.getBytes());
+		}
+	};
 	
 }
